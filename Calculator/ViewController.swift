58003ca80fdd6dc2718c5270b7c0a9749d1bef6e
Karina Macancela <karymacancela@gmail.com>
2019-04-24 23:42:40 -0400

    Added operation buttons to view controller

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,9 +14,27 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
     
+    
     @IBAction func numbers(_ sender: UIButton) {
         label.text = label.text! + String(sender.tag - 1)
         numberOnScreen = Double(label.text!)!
+    }
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        if label.text != "" && sender.tag != 11 && sender.tag != 16 {
+            if sender.tag == 12 {
+                
+            }
+            else if sender.tag == 13 {
+                
+            }
+            else if sender.tag == 14 {
+                
+            }
+            else if sender.tag == 15 {
+                
+            }
+        }
     }
     
     override func viewDidLoad() {
